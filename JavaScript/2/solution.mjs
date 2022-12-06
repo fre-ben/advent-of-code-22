6005fcaeec3734a12f1adb7e317e6bde6cbced16
frederik.schocke <frederik.schocke@aoe.com>
2022-12-06 15:25:43 +0100

    feat: add notes to day 2 solution

--- JavaScript/2/solution.mjs
+++ JavaScript/2/solution.mjs
@@ -5,3 +5,18 @@ import fs from "fs";
 const inputData = fs.readFileSync(new URL("./input.txt", import.meta.url), "utf8", (data) => {
   return data;
 });
+
+// Notes:
+// A & X = ROCK -> 1 Point
+// B & Y = PAPER -> 2 Points
+// C & Z = SCISSORS -> 3 Points
+// A & X > C & Z
+// B & Y > A & X
+// C & Z > B & Y
+// Win: 6 Points
+// Lose: 0 Points
+// Draw: 3 Points
+
+// Array aus Match-Strings machen und per reducer auf Punkte runterbrechen, danach Punkte Array summieren.
+
+console.log(inputData.split("\n"));
