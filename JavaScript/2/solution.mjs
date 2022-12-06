1d0f8b90a1f8d1665260279885b501eb2d133c96
frederik.schocke <frederik.schocke@aoe.com>
2022-12-06 12:02:28 +0100

    chore: test commit

--- JavaScript/2/solution.mjs
+++ JavaScript/2/solution.mjs
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-// Task Part 1: What would your total score be if everything goes exactly according to your strategy guide?
+// Task Part 1: What would your total score be if everything goes exactly according to your strategy guide ?
 
 const inputData = fs.readFileSync(new URL("./input.txt", import.meta.url), "utf8", (data) => {
   return data;
