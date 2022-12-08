4d9a847f067c080b7996d1da6372120666ff5b8e
frederik.schocke <frederik.schocke@aoe.com>
2022-12-08 09:58:33 +0100

    feat: add solution for day2 part 2

--- JavaScript/2/solution.mjs
+++ JavaScript/2/solution.mjs
@@ -18,6 +18,14 @@ const inputData = fs.readFileSync(new URL("./input.txt", import.meta.url), "utf8
   return data;
 });
 
+const winCondition = (a, b) => (a === 1 && b === 3) || (a === 2 && b === 1) || (a === 3 && b === 2);
+
+const winningMatchup = [
+  [3, 1],
+  [1, 2],
+  [2, 3],
+];
+
 /**
  * Gets the correct score based on the given shape.
  * @param {string} shape
@@ -48,7 +56,7 @@ function getOutcomeScore(round) {
   const request = getShapeScore(round[0]);
   const response = getShapeScore(round[1]);
 
-  if ((response === 1 && request === 3) || (response === 2 && request === 1) || (response === 3 && request === 2)) {
+  if (winCondition(response, request)) {
     return 6;
   } else if (request === response) {
     return 3;
@@ -57,13 +65,38 @@ function getOutcomeScore(round) {
   }
 }
 
-// Todo: FUnktion, die die rounds basierend auf das gewünschte Endergebnis transformiert und den response Wert entsprechend austauscht
-// Danach vorhandene Funktionen abfeuern
+/**
+ * Calculates the roundscore based on the strategy guide
+ * @param {[string, string]} round
+ * @returns {number} roundScore
+ */
+function calculateStrategyScore(round) {
+  const request = getShapeScore(round[0]);
+  const responseStrategy = round[1];
+
+  switch (responseStrategy) {
+    // Win
+    case "Z":
+      return winningMatchup.find((matchUp) => matchUp[0] === request)[1] + 6;
+    // Draw
+    case "Y":
+      return request + 3;
+    // Lose
+    case "X":
+      return winningMatchup.find((matchUp) => matchUp[1] === request)[0];
+    default:
+      throw new Error("unknown response strategy");
+  }
+}
 
 const games = inputData.split("\n");
 const rounds = games.map((game) => game.split(" "));
 
-const totalRoundScores = rounds.map((round) => getOutcomeScore(round) + getShapeScore(round[1]));
-const totalScore = totalRoundScores.reduce((a, b) => a + b, 0);
+const totalRoundScoresPart1 = rounds.map((round) => getOutcomeScore(round) + getShapeScore(round[1]));
+const totalScorePart1 = totalRoundScoresPart1.reduce((a, b) => a + b, 0);
 
-console.log(`Part 1: If everything goes according to my strategy guide, my total score would be ${totalScore}.`);
+const totalRoundScoresPart2 = rounds.map((round) => calculateStrategyScore(round));
+const totalScorePart2 = totalRoundScoresPart2.reduce((a, b) => a + b, 0);
+
+console.log(`Part 1: If everything goes according to my strategy guide, my total score would be ${totalScorePart1}.`);
+console.log(`Part 2: If everything goes according to my strategy guide, my total score would be ${totalScorePart2}.`);
